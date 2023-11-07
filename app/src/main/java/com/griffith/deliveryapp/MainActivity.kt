6bbbf016dcebc81515b5bb443ab812a2f2fc3607
Albert Kison <albert.kison@student.griffith.ie>
2023-11-07 01:29:57 +0000

    Add a restaurant card and intent from main activity to restaurant detai
    ls

--- app/src/main/java/com/griffith/deliveryapp/MainActivity.kt
+++ app/src/main/java/com/griffith/deliveryapp/MainActivity.kt
@@ -1,28 +1,55 @@
 package com.griffith.deliveryapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.griffith.deliveryapp.ui.theme.DeliveryAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val context = LocalContext.current
+
             DeliveryAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        ResCard(onClick = {
+                            val intent = Intent(context, ResDetails::class.java)
+                            startActivity(intent)
+                        })
+                    }
+                    
                 }
             }
         }
@@ -35,6 +62,36 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+}
+
+@Composable
+fun ResCard(onClick: () -> Unit) {
+    Card (
+        elevation = CardDefaults.cardElevation(5.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White),
+        modifier = Modifier.clickable { onClick() }
+    ) {
+        Column {
+            Image(
+                painter = painterResource(R.drawable.res1),
+                contentDescription = null, // decorative
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .height(150.dp)
+                    .fillMaxWidth()
+            )
+//                                    Spacer(modifier = Modifier.height(4.dp))
+
+            Column (modifier = Modifier.padding(16.dp, 10.dp, 16.dp, 10.dp)) {
+                Text(
+                    text = "Bleeding Horse",
+                    fontSize =  24.sp,
+                    style = MaterialTheme.typography.titleLarge,
+                    fontWeight = FontWeight.SemiBold
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
